59c9399170285ab2f2388db269c51ad750a99dcb
gernotf <gernot.fussan@gmail.com>
2023-04-07 17:35:44 +1000

    add fields 2

--- src/com/fussoft/monopoly/AustraliaBoard.java
+++ src/com/fussoft/monopoly/AustraliaBoard.java
@@ -167,6 +167,182 @@ public class AustraliaBoard {
 				0, //rentHouse4
 				0  //rentHotel
 			);
+
+		FIELDS[10] = new AustraliaBoardField(
+				"Jail Just Visiting",
+				FIELD_TYPE.VISITING_JAIL,
+				null,
+				0, //value
+				0, //sameColorCount
+				0, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[11] = new AustraliaBoardField(
+				"Margret River",
+				FIELD_TYPE.LOCATION,
+				COLOR_CODE.PINK,
+				140, //value
+				3, //sameColorCount
+				100, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[12] = new AustraliaBoardField(
+				"Electric Company",
+				FIELD_TYPE.WORKS,
+				null,
+				150, //value
+				0, //sameColorCount
+				0, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[13] = new AustraliaBoardField(
+				"Broome",
+				FIELD_TYPE.LOCATION,
+				COLOR_CODE.PINK,
+				140, //value
+				3, //sameColorCount
+				100, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[14] = new AustraliaBoardField(
+				"Esperance",
+				FIELD_TYPE.LOCATION,
+				COLOR_CODE.PINK,
+				160, //value
+				3, //sameColorCount
+				100, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[15] = new AustraliaBoardField(
+				"Brisban Airport",
+				FIELD_TYPE.AIRPORT,
+				null,
+				200, //value
+				0, //sameColorCount
+				0, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[16] = new AustraliaBoardField(
+				"Phillip Island",
+				FIELD_TYPE.LOCATION,
+				COLOR_CODE.ORANGE,
+				180, //value
+				3, //sameColorCount
+				100, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[17] = new AustraliaBoardField(
+				"Community Chest",
+				FIELD_TYPE.COMMUNITY_CHEST,
+				null,
+				0, //value
+				0, //sameColorCount
+				0, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[18] = new AustraliaBoardField(
+				"Melbourne",
+				FIELD_TYPE.LOCATION,
+				COLOR_CODE.ORANGE,
+				180, //value
+				3, //sameColorCount
+				100, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[19] = new AustraliaBoardField(
+				"Great Ocean Road",
+				FIELD_TYPE.LOCATION,
+				COLOR_CODE.ORANGE,
+				200, //value
+				3, //sameColorCount
+				100, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
+
+		FIELDS[20] = new AustraliaBoardField(
+				"Free Parking",
+				FIELD_TYPE.FREE_PARKING,
+				null,
+				0, //value
+				0, //sameColorCount
+				0, //priceHouseAndHotel
+				0, //rentSolo
+				0, //rentAllColor
+				0, //rentHouse1
+				0, //rentHouse2
+				0, //rentHouse3
+				0, //rentHouse4
+				0  //rentHotel
+			);
 	}
 
 }
